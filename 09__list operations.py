691359b0673fdced8e765b3c9f22ab9838d920e7
Prity Raj <prsinha1500@gmail.com>
2026-04-18 22:08:10 +0530

    Add files via upload

---
+++ 09__list operations.py
@@ -0,0 +1,34 @@
+#Perform all list operations: insert, append, extend, remove, 
+#pop, sort, reverse.
+
+l = [3, 1, 4]
+
+print("Original List:", l)
+
+# append()
+l.append(5)
+print("After append:", l)
+
+# insert()
+l.insert(1, 10)
+print("After insert:", l)
+
+# extend()
+l.extend([6, 7])
+print("After extend:", l)
+
+# remove()
+l.remove(10)
+print("After remove:", l)
+
+# pop()
+l.pop()
+print("After pop:", l)
+
+# sort()
+l.sort()
+print("After sort:", l)
+
+# reverse()
+l.reverse()
+print("After reverse:", l)
