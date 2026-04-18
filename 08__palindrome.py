a27778307ba14f19353005b01bd5730c0c4dbe5b
Prity Raj <prsinha1500@gmail.com>
2026-04-18 22:04:36 +0530

    Add files via upload

---
+++ 08__palindrome.py
@@ -0,0 +1,12 @@
+# Check whether a string is a palindrome (with and without slicing).
+
+s = input("Enter a string: ")
+rev = ""
+
+for ch in s:
+    rev = ch + rev   # reverse string manually
+
+if s == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
