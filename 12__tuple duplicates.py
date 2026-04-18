145926fea4f1b3385ea037d2b6cf5852ab79755f
Prity Raj <prsinha1500@gmail.com>
2026-04-18 22:22:22 +0530

    Delete 12__tuple duplicates.py

--- 12__tuple duplicates.py
+++
@@ -1,9 +0,0 @@
-# Remove duplicates from a list while maintaining order.
-lst = [10, 5, 8, 10, 5, 3, 8]
-
-result = []
-for item in lst:
-    if item not in result:
-        result.append(item)
-
-print("List after removing duplicates:", result)
