0c1a437d368b296404424c91490a5639e9bef51f
Prity Raj <prsinha1500@gmail.com>
2026-04-19 12:53:22 +0530

    Add files via upload

---
+++ 17__copying file contents.py
@@ -0,0 +1,9 @@
+with open("E:\\sample.txt", "r") as f1, open("E:\\output.txt", "w") as f2:
+    for line in f1:
+        new_line = ""
+        for ch in line:
+            if not ch.isdigit():   # skip digits
+                new_line += ch
+        f2.write(new_line)
+
+print("File copied successfully without digits.")
