626c3654a18329379f24719443494158dbb2cb0c
Prity Raj <prsinha1500@gmail.com>
2026-04-19 14:04:57 +0530

    Refactor file copying to remove digits

--- 17__copying file contents.py
+++ 17__copying file contents.py
@@ -1,3 +1,4 @@
+# 17.  Copy contents of one file to another after removing digits.
 with open("E:\\sample.txt", "r") as f1, open("E:\\output.txt", "w") as f2:
     for line in f1:
         new_line = ""
